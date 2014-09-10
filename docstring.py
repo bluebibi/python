25548e9772a14f6862878f14f01df3d355b8163b
Youn-Hee Han <yh21.han@gmail.com>
2014-09-10 17:05:44 +0900

    no message

---
+++ docstring.py
@@ -0,0 +1,12 @@
+#file docstring.py
+'''
+docstring module provide you with a fun thing.
+line1
+line2
+'''
+
+class Ham:
+	"Ham class defines a new things"
+	def func(self):
+		"func function in Ham class executes its internal things"
+		pass
