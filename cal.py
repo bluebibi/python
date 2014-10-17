c3557071c9b566f799c087ece3ce0b31f538843b
Youn-Hee Han <yh21.han@gmail.com>
2014-10-17 13:44:07 +0900

    no message

--- cal.py
+++ cal.py
@@ -1,3 +1,15 @@
-# file: cal.py
-import calendar
-calendar.prmonth(2014, 9)
+A Confidence-based Multicast Scheme for Mobile Physical Activity Monitoring
+Sensor-based Detection of Anomalous Patterns in Home Networks
+Dynamic Sensor Events Analysis for Real-time Activity in a Smart Home Context
+An Empirical Analysis of Mobile Smart Medical Devices in Mental Disorder Treatment
+A Secured Access Control Scheme for Vehicles in RFID Systems on Express Ways
+An Efficient Digital Library Service in Mobile and Cloud Computing Systems
+A Utility Maximization Scheme for Communication Tradeoff in Wireless Body Area Networks
+An Efficient Load Distribution Mechanism in Smart Grid Networks
+An Efficient Performance Monitoring and Evaluation with Mobile Sensing Technology
+A Proposal of High-performance and Energy-efficient Vehicular Sensing Platform
+A Survey of Energy Efficient Urban Vehicular Sensing Platforms
+Physically-based Numerical Models of Low-power Wireless Links through Simulation
+An Active Time Scheduling for Rotatable Sensor Networks
+Using Traffic Asymmetry to Enhance TCP Performance in Wireless BANs
+A Cyclic Stable Matching Scheme for Three-sided Networking Services in Sensor Networks
