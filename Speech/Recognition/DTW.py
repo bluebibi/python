718600609c135f3ffeaf5f25db1c35031ce4359c
Youn-Hee Han <yh21.han@gmail.com>
2014-11-10 21:47:58 +0900

    no message

---
+++ Speech/Recognition/DTW.py
@@ -0,0 +1,3 @@
+def warping():
+    print "warping"
+    pass
---
+++ Speech/Recognition/HMM.py
@@ -0,0 +1,14 @@
+def train():
+    print "Train"
+    pass
+
+def loadModel():
+    print "LoadModel"
+    pass
+
+def saveModel():
+    print "SaveModel"    
+    pass
+
+import DTW
+DTW.warping()
